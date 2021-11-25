1a2d89be1499d11d89ed36f978cc4eac95601cb8
Hadi Safari <79026810+hadisafari77@users.noreply.github.com>
2021-11-25 05:36:56 -0800

    Update scores.js

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,1 +1,26 @@
-scores.js
+
+// display data on highscores page
+
+var highScores = JSON.parse(localStorage.getItem("scores")) 
+
+var highScoreEL = document.getElementById("highScores")
+
+for (i=0 ; i < highScores.length-1; i++) { 
+var highScore = highScores[i]
+var li = document.createElement("li")  
+li.innerText = highScore.initials + " " + highScore.score + " "
+highScoreEL.appendChild(li) 
+
+
+// removing data from Local Storage
+  function clearhighScores() {
+    localStorage.removeItem("scores");
+    location.reload();
+  }
+
+  document.getElementById("clearScores").onclick = clearhighScores;
+
+  }
+
+
+
