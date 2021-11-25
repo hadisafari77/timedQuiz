8a7950262477c34b98c5f346fbf90346811cac52
Hadi Safari <79026810+hadisafari77@users.noreply.github.com>
2021-11-25 05:36:23 -0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ var questionsEl = document.getElementById("questions");
 document.getElementById("startbutton").addEventListener("click", function () {
   // hide quiz start area 
   document.getElementById("startarea").style.display = "none"
-  
+
   // hide footer 
   document.getElementById("bottom-page").style.display = "none"
 
@@ -22,10 +22,10 @@ document.getElementById("startbutton").addEventListener("click", function () {
 
 })
 
-
+var timerInterval
 // Timer Function
 function setTime() {
-  var timerInterval = setInterval(function () {
+  timerInterval = setInterval(function () {
     time--;
 
     document.getElementById("time").innerText = time
@@ -40,11 +40,6 @@ function setTime() {
 
 }
 
-// Quiz end function + interval clearing
-function quizEnd() {
-  clearInterval(timerInterval); 
-}
-
 console.log(questions)
 
 // Rendering Questions
@@ -54,74 +49,80 @@ function renderQuestions() {
 
   document.getElementById("question-title").innerText = questions[currentquestions].title
 
-
   for (let i = 0; i < questions[currentquestions].choices.length; i++) {
 
     document.getElementById(i + 1).innerText = questions[currentquestions].choices[i]
 
   }
-
 }
+
+// Submit highscore to local storage after quiz end
+document.getElementById("submitBtn").addEventListener("click", function (event) {
+
+  var scoreInput = document.getElementById("scoreInput").value
+  var previousScores = JSON.parse(localStorage.getItem("scores"))
+
+  if (previousScores === null) {
+    previousScores = []
+
+  }
+
+  var userScore = {
+    initials: scoreInput,
+    score: time
+
+  }
+  previousScores.push(userScore)
+  localStorage.setItem("scores", JSON.stringify(previousScores))
+  window.location.replace("highscores.html")
+})
 
 
 // Rendering choices for different questions
 document.getElementById("choices").addEventListener("click", function (event) {
   console.log(event.target.innerText)
+
   if (currentquestions > questions.length - 2) {
 
+    // Quiz end function + interval clearing
+    quizEnd()
     return
   }
+
+  // checking for correct answer
   if (event.target.innerText === questions[currentquestions].answer) {
 
     console.log("correct")
 
   }
 
+  // checking for wrong answer
   else {
     console.log("wrong")
     time = time - 10
 
   }
- 
+
+  // render the next question
   currentquestions = currentquestions + 1
 
+  //returning the function
   renderQuestions()
 })
 
-
+// Quiz End function
 function quizEnd() {
-    // stop timer
-    clearInterval(timerInterval);
+  // stop timer
+  clearInterval(timerInterval);
 
-    // show end screen
-    document.getElementById("ending-screen");
-    document.removeAttribute("class");
+  // show end screen
+  document.getElementById("ending-screen").classList.remove("hide");
 
-    // // show final score
-    // var finalScoreEl = document.getElementById("final-score");
-    // finalScoreEl.textContent = time;
+  // hide questions section
+  document.getElementById("questions").setAttribute("class", "hide");
 
-    // hide questions section
-    currentquestions.setAttribute("class", "hide");
-  }
+  document.getElementById("final-score").innerText = time
+  
+}
 
-function clockTick() {
-    // update time
-    time--;
-    timerInterval.textContent = time;
-
-    // check if user ran out of time
-    if (time <= 0) {
-      quizEnd();
-    }
-  }
-
-
-
-
-
-
-// localStorage.setItem("name", "lastname")
-
-// localStorage.getItem("name")
 
